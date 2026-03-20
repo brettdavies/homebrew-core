ddb8f6947b38bf3bd81e587f974420cd7ced6d4b
Mike McQuaid <mike@mikemcquaid.com>
2026-03-20 11:36:26 +0000

    libspatialite: add compatibility_version 1
    A `compatibility_version` backfill for formulae with dependents.

--- Formula/lib/libspatialite.rb
+++ Formula/lib/libspatialite.rb
@@ -4,6 +4,8 @@ class Libspatialite < Formula
   license any_of: ["MPL-1.1", "GPL-2.0-or-later", "LGPL-2.1-or-later"]
   revision 4
 
+  compatibility_version 1
+
   stable do
     url "https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-5.1.0.tar.gz"
     mirror "https://ftp.netbsd.org/pub/pkgsrc/distfiles/libspatialite-5.1.0.tar.gz"
